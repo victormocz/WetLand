f6db159aa0dc6ab4316421ed3c04b822519e77b7
Mo <victormocz@live.com>
2016-03-17 22:48:38 -0500

    sort NBData 15

--- WetLand/KSTest/KResult.cs
+++ WetLand/KSTest/KResult.cs
@@ -14,7 +14,7 @@ namespace WetLand.KSTest
         public double ks2stat;
         public double p;
         public List<double> Interp_BData;
-        public List<double> NBData;
+        public double[] NBData;
         public KResult(string name,List<double> listB,List<double> originalList1, List<Double> originalList2, List<Double> sortedList1, List<Double> sortedList2) {
             if (originalList1.Count == 0 || originalList2.Count == 0 || sortedList1.Count == 0 || sortedList2.Count == 0) {
                 throw new Exception("Length of Interp_B and NB dataset Should greater than 0");
@@ -30,10 +30,12 @@ namespace WetLand.KSTest
             }
             this.name = name;
             this.Interp_BData = listB;
-            this.NBData = originalList2;
+            //TODO debug
             double[] originalArray1 = originalList1.ToArray();
             double[] originalArray2 = originalList2.ToArray();
             TwoSampleKolmogorovSmirnovTest ktest = new TwoSampleKolmogorovSmirnovTest(originalArray1, originalArray2);
+            Array.Sort(originalArray2);
+            this.NBData = originalArray2;
             this.ks2stat = ktest.Statistic;
             this.p = ktest.PValue;
             
